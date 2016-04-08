20e91f29e55eea0b2dd10c4128f7e03bd355ed01
Johann BICH <johannbich@gmail.com>
2016-04-08 10:11:05 +0800

    openshift config

--- config/local.js
+++ config/local.js
@@ -12,5 +12,5 @@ module.exports={
   host: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
   explicitHost: process.env.OPENSHIFT_APP_DNS || "localhost",
   port: process.env.OPENSHIFT_NODEJS_PORT || 1337,
-  environment: process.env.NODE_ENV || 'development'
+  environment: process.env.OPENSHIFT_MONGODB_DB_HOST ? 'production' : 'development'
 };
